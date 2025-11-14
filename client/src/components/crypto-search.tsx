985c1de71dc2464fec83dec4efa028a66168daed
Juan <juandiego.velez@utp.edu.co>
2025-11-14 15:45:25 -0500

    feat: agregar buscador de crypto

---
+++ client/src/components/crypto-search.tsx
@@ -0,0 +1,34 @@
+import { Kbd } from "@heroui/kbd";
+import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
+import { Link } from "@heroui/link";
+
+import { SearchIcon } from "@/components/icons";
+import { useSocket } from "@/context/SocketContext";
+
+export const CryptoSearch = () => {
+  const { top50 } = useSocket();
+
+  return (
+    <Autocomplete
+      aria-label="Search"
+      endContent={
+        <Kbd className="hidden lg:inline-block" keys={["command"]}>
+          K
+        </Kbd>
+      }
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+      }
+      type="search"
+    >
+      {top50.map((crypto) => (
+        <AutocompleteItem key={crypto.name}>
+          <Link className="w-full text-normal" href={`/${crypto.id}`}>
+            {crypto.name}
+          </Link>
+        </AutocompleteItem>
+      ))}
+    </Autocomplete>
+  );
+};
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,6 +1,4 @@
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import {
   Navbar as HeroUINavbar,
   NavbarBrand,
@@ -11,37 +9,15 @@ import {
   NavbarMenuItem,
 } from "@heroui/navbar";
 import { link as linkStyles } from "@heroui/theme";
-import { useLocation } from "react-router-dom";
 import clsx from "clsx";
+
+import { CryptoSearch } from "./crypto-search";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import { GithubIcon, SearchIcon } from "@/components/icons";
+import { GithubIcon } from "@/components/icons";
 
 export const Navbar = () => {
-  const location = useLocation();
-
-  const searchInput = location.pathname === "/" && (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -84,7 +60,9 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <CryptoSearch />
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -96,7 +74,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        <CryptoSearch />
 
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
